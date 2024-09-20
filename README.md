893c88cadcc3c01cbf1b00573573e95a5a51ec51
dogman103 <snowprincev2@gmail.com>
2024-09-19 23:42:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project4_connorcole
+ project 4 for CSCI-2910; using a web api
---
+++ data_model.py
@@ -0,0 +1,68 @@
+import requests
+import datetime
+from pydantic import BaseModel, ValidationError
+from enum import Enum
+
+class role(BaseModel):
+  uuid : str
+  displayName  :str 
+  description : str 
+  displayIcon : str
+  assetPath : str
+
+class recruitmentData(BaseModel):
+  recruitmentData : object 
+  counterId : str
+  milestoneId : str
+  milestoneThreshold : int
+  useLevelVpCostOverride : bool
+  levelVpCostOverride : int
+  startDate : datetime
+  endDate : datetime
+
+class abilities(BaseModel):
+  slot : str
+  displayName : str
+  description : str 
+  displayIcon : str
+
+class mediaList(BaseModel):
+  id : int
+  wwise :str
+  wave : str
+
+class voiceLine(BaseModel):
+  minDuration : float
+  maxDuration : float
+  mediaList : mediaList
+
+class data(BaseModel):
+  uuid : str
+  displayName : str 
+  description : str 
+  developerName: str
+  characterTags : list[str]
+  displayIcon:  str
+  displayIconSmall : str
+  bustPortrait : str
+  fullPortrait : str
+  fullPortraitV2: str
+  killfeedPortrait: str
+  background: str
+  backgroundGradientColors: list[str]
+  assetPath : str
+  isFullPortraitRightFacing : bool
+  isPlayableCharacter : bool
+  isAvailableForTest : bool
+  isBaseContent : bool
+  role : role
+  recruitmentData : recruitmentData
+  abilities : list[abilities]
+  voiceLine : voiceLine
+
+
+class Agent(BaseModel):
+  status : int
+  data : data
+      
+        
