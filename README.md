6a3087758669bc8d3e9449212f1217512e1e83e8
dogman103 <snowprincev2@gmail.com>
2024-09-25 13:29:27 -0400

    cleaning + bundles
    cleaned up main, added bundle functionality and another API call

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # project4_connorcole
  project 4 for CSCI-2910; using a web api
-
- im not even gonna lie i started this while working on the documentation and got like 30 minutes deep making the model for the data before i remembered to make a repo. this shit is all hand typed i had to take a walk around campus at 11:00 at NIGHT on a thursday evening to figure it out
+ blood sweat and tears has gone into this but i'm starting to enjoy it and understand it.
 
 
  
@@ -13,10 +12,9 @@
   - DATA MODELS ARE UNTESTED!!!!!!
     - Data models do not work
   - no clue if API will actually call and return good data
+  - Need to get bundle info...
 
 ## Notes
-  - the data model was made over the span of like an hour (and remains untested)
-  - i am not adding more data models than just AGENTS. this has too many damn attributes for each thing. i dont want to do any more (11:49 PM, 9/19/24)
-  -i am done for the night (11:50 PM, 9/19/24)
   - after blood, sweat, and tears, i figured it out. fucking nested dictionaries. that's why it wouldn't work- it was already a dictionary of dictionaries. (2:43 PM, 9/24/24)
+  - API calls all work, data is good and is always validated, no issues currently. just need to implement rest of program (1:25 PM, 9/25/2024)
 
--- main.py
+++ main.py
@@ -2,84 +2,8 @@ import requests
 from datetime import datetime
 from pydantic import BaseModel, ValidationError
 import json
+from data_model import Agent, Bundle
 
-class role(BaseModel):
-  uuid : str
-  displayName  :str 
-  description : str 
-  displayIcon : str
-  assetPath : str
-
-class recruitmentData(BaseModel):
-  recruitmentData : object 
-  counterId : str
-  milestoneId : str
-  milestoneThreshold : int
-  useLevelVpCostOverride : bool
-  levelVpCostOverride : int
-  startDate : datetime
-  endDate : datetime
-
-class abilities(BaseModel):
-  slot : str
-  displayName : str
-  description : str 
-  displayIcon : str
-
-class mediaList(BaseModel):
-  id : int
-  wwise :str
-  wave : str
-
-class voiceLine(BaseModel):
-  minDuration : float
-  maxDuration : float
-  mediaList : list[mediaList]
-
-class agentData(BaseModel):
-  uuid : str
-  displayName : str
-  description : str 
-  developerName: str
-  characterTags : list[str] | None
-  displayIcon:  str
-  displayIconSmall : str
-  bustPortrait : str
-  fullPortrait : str
-  fullPortraitV2: str
-  killfeedPortrait: str
-  background: str
-  backgroundGradientColors: list[str]
-  assetPath : str
-  isFullPortraitRightFacing : bool
-  isPlayableCharacter : bool
-  isAvailableForTest : bool
-  isBaseContent : bool
-  role : role
-  recruitmentData : recruitmentData | None
-  abilities : list[abilities]
-  voiceLine : voiceLine | None
-
-class bundleData(BaseModel):
-  uuid : str
-  displayName : str
-  displayNameSubtext : str
-  description : str
-  extraDescription : str
-  promoDescription : str 
-  useAdditionalContext : bool
-  displayIcon : str
-  displayIcon2 : str
-  logoIcon : str
-  verticalPromoImage : str
-  assetPath : str
-
-class Agent(BaseModel):
-  data : agentData
-
-class Bundle(BaseModel):
-
-  data : bundleData
 
 agents = {}
 response = requests.get("https://valorant-api.com/v1/agents")
@@ -88,7 +12,7 @@ try:
     agents = response.json()
     print("data is valid")
 except ValidationError:
-  print("Validation error! try again idk man")  
+  print("Agent Validation error! try again idk man")  
 
 gekko : Agent = agents['data'][0]
 gekkoUuid = gekko['uuid']
@@ -97,14 +21,14 @@ agentNamesInOrder = []
 for agent in agents['data']:
   agentNamesInOrder.append(agent['displayName'])
 
-
-
-#example by uuid
-#specificAgent = requests.get("https://valorant-api.com/v1/agents/e370fa57-4757-3604-3648-499e1f642d3f")
-#gdata = specificAgent.json()
-#print(gdata)
-#gekko = Agent(**gdata)
-#print(gekko)
+bundles = {}
+bundleResponse = requests.get("https://valorant-api.com/v1/bundles")
+try:
+  if bundleResponse.status_code == 200:
+    bundles = bundleResponse.json()
+    print("valid data")
+except ValidationError:
+  print("Bundle validation error; try again")
 
 agentNumber = 0
 
@@ -112,30 +36,39 @@ while(True):
   print("\nPress 1 to continue to agent select; press 0 to exit")
   flag = input()
   if (flag == '1'):
-    print("\nPlease select an agent to view:")
-    for agent in agentNamesInOrder:
-      print(agentNumber, agent)
-      agentNumber += 1
-    agentSelection=int(input("\nEnter the number of the agent to select: "))
-    agentName = agents['data'][agentSelection]['displayName']
-    print("what would you like to view?")
-    print(f"1. The description of {agentName}")
-    print(f"2. The abilities of {agentName}")
-    print(f"3. All the information about {agentName}")
-    characterInput = input("Select one of the options above and hit enter: ")
-    if (characterInput == "1"):
-      print('\n')
-      print(agents['data'][agentSelection].get("description"))
-    elif (characterInput == "2"):
-      print('\n')
-      agentAbilities = agents['data'][agentSelection].get('abilities')
-      for item in agentAbilities:
-        print(f"{item.get('displayName')} : {item.get('description')}")
-    elif (characterInput == "3"):
-      for item in agents['data'][agentSelection].items():
-        print(item)
-    else:
-      print("Input not recognized; try again!")
-    agentNumber = 0
+    print("\n Would you like to view agents or bundles?: ")
+    whichOne = input("0 for Agents, 1 for Bundles: ")
+    if (whichOne == '0'):
+      print("\nPlease select an agent to view:")
+      for agent in agentNamesInOrder:
+        print(agentNumber, agent)
+        agentNumber += 1
+      agentSelection=int(input("\nEnter the number of the agent to select: "))
+      agentName = agents['data'][agentSelection]['displayName']
+      print("what would you like to view?")
+      print(f"1. The description of {agentName}")
+      print(f"2. The abilities of {agentName}")
+      print(f"3. All the information about {agentName}")
+      characterInput = input("Select one of the options above and hit enter: ")
+      if (characterInput == "1"):
+        print('\n')
+        print(agents['data'][agentSelection].get("description"))
+      elif (characterInput == "2"):
+        print('\n')
+        agentAbilities = agents['data'][agentSelection].get('abilities')
+        for item in agentAbilities:
+          print(f"{item.get('displayName')} : {item.get('description')}")
+      elif (characterInput == "3"):
+        for item in agents['data'][agentSelection].items():
+          print(item)
+      else:
+        print("Input not recognized; try again!")
+      agentNumber = 0
+    elif (whichOne == '1'):
+      bundleCount = 0
+      for item in bundles['data']:
+        print(f"{bundleCount} : {bundles['data'][bundleCount]['displayName']}")
+        bundleCount += 1
+      bundleCount = 0
   else:
     break
